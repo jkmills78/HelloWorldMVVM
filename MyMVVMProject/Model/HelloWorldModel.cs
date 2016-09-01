85996c73f3e93dab7541c3998f832b83a9b26299
jkmills78 <jkmills78@outlook.com>
2016-09-01 07:18:20 -0400

    Add project files.

---
+++ MyMVVMProject/Model/HelloWorldModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldMVVM.Model
+{
+    public class HelloWorldModel
+    {
+        private List<string> repositoryData;
+        public string ImportantInfo
+        {
+            get
+            {
+                return ConcatenateData(repositoryData);
+            }
+        }
+
+        public HelloWorldModel()
+        {
+            repositoryData = GetData();
+        }
+
+        /// <summary>
+        /// Simulates data retrieval from a repository
+        /// </summary>
+        /// <returns>List of strings</returns>
+        private List<string> GetData()
+        {
+            repositoryData = new List<string>()
+            {
+                "Hello",
+                "world"
+            };
+            return repositoryData;
+        }
+
+        /// <summary>
+        /// Concatenate the information from the list into a fully formed sentence.
+        /// </summary>
+        /// <returns>A string</returns>
+        private string ConcatenateData(List<string> dataList)
+        {
+            string importantInfo = dataList.ElementAt(0) + ", " + dataList.ElementAt(1) + "!";
+            return importantInfo;
+        }
+    }
+}
---
+++ MyMVVMProject/ViewModel/HelloWorldViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using HelloWorldMVVM.Model;
+
+namespace HelloWorldMVVM.ViewModel
+{
+    // Implements INotifyPropertyChanged interface to support bindings
+    public class HelloWorldViewModel : INotifyPropertyChanged
+    {
+        private string helloString;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string HelloString
+        {
+            get
+            {
+                return helloString;
+            }
+            set
+            {
+                helloString = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Raises OnPropertychangedEvent when property changes
+        /// </summary>
+        /// <param name="name">String representing the property name</param>
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public HelloWorldViewModel()
+        {
+            HelloWorldModel helloWorldModel = new HelloWorldModel();
+            helloString = helloWorldModel.ImportantInfo;
+        }
+    }
+}
